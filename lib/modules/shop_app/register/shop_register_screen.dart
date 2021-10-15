11de691050d6a639036570a38a78a2e5662b0c66
eslamreda55 <eslam.r.elsayed55@gmail.com>
2021-10-15 22:15:27 +0200

    udate

--- lib/modules/shop_app/register/shop_register_screen.dart
+++ lib/modules/shop_app/register/shop_register_screen.dart
@@ -6,7 +6,7 @@ class ShopRegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Eslam Market"),
+        title: Text("Eslam"),
       ),
     );
   }
